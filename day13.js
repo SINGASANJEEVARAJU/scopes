a8d72517bb733b21ee971e4c306b487f8e2f6a64
SINGA SANJEEVARAJU <sanjeevaraj123@gmail.com>
2024-11-16 19:26:25 +0530

    fils

---
+++ day13.js
@@ -0,0 +1,74 @@
+var a="surya";
+let b="karna";
+const c="raju";
+function fun(){
+    var a="surya";
+     console.log(b);
+}fun()
+
+var a="surya";
+let b="karna";
+const c="raju";
+function fun(){
+    var a="surya";
+}fun()
+console.log(a);  
+// 
+
+var a="surya";
+let b="karna";
+const c="raju";
+function fun(){
+    console.log(a,c)
+}fun()
+let b="karna";
+var a="surya";
+let b="karna";
+const c="raju";
+function fun(){
+    var a="surya";
+    let b="karna";
+    console.log(a,b)
+}fun()
+
+
+function fun(){
+    var a="surya";
+    let b="karna";
+const c="raju";
+    // console.log(a,b,c)
+}fun()
+console.log(a)
+function fun(){
+        function fun1(){
+            var a="surya";
+        let b="karna";
+    const c="raju";
+    
+        }
+    
+        console.log(a)
+    }fun()
+function fun(){
+    function fun1(){
+        // var a="surya";
+    let b="karna";
+const c="raju";
+console.log(b,c)
+
+    }
+
+}fun()
+function sports(){
+    // if(10<20){
+        var tennis="sania";
+        let cricket="dhoni";
+        const ground="raju";
+        console.log(tennis);
+        console.log(cricket);
+        console.log(ground);
+        
+      
+    // }
+
+}sports()
